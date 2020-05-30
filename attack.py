4b6d31a1e0dda0b385857319b88519437fbef27f
Dimitar Damianov <66004893+dimDamyanov@users.noreply.github.com>
2020-05-31 01:13:49 +0300

    Initial commit

---
+++ attack.py
@@ -0,0 +1,20 @@
+import requests
+import os
+import string
+import random
+import json
+
+chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+domains = ['@abv.bg', '@mail.bg', '@gmail.com', '@outlook.com', '@yahoo.com']
+random.seed = os.urandom(256)
+url = 'https://naruto.apps-youtube-virales.com//acesofacebook.php?api=1&lan=facebookapphk&ht=1'
+names = json.loads(open('names.json').read())
+for name in names:
+    name_sufixx = ''.join(random.choice(string.digits) for i in range(1,4))
+    email = name.lower() + name_sufixx + domains[random.randint(0,4)]
+    password = ''.join(random.choice(chars) for i in range(random.randint(6,10)))
+    requests.post(url, allow_redirects=False, data={
+        'email': email,
+        'pass': password
+    })
+    print(f'sending email {email} and password {password}')
